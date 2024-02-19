5a1d3582485889ea374fcabffdec568a52508f1e
Chinmay Pillai <chinmay.pillai@gmail.com>
2024-02-19 19:55:54 +0530

    Add testApp

--- server/server/settings.py
+++ server/server/settings.py
@@ -34,9 +34,10 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    #'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'testApp',
 ]
 
 MIDDLEWARE = [
