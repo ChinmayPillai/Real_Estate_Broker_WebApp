c3ac1f216533edd10ffacaf1fa0c2172689fadd8
Chinmay Pillai <chinmay.pillai@gmail.com>
2024-02-25 21:58:03 +0530

    Setup MySQL DB

--- server/server/settings.py
+++ server/server/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'real_estate_broker',
+        'USER': 'dev',
+        'PASSWORD': '123',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
