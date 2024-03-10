662dbc00a141f6ed657912e60f78431b80ebcaa9
Chinmay Pillai <chinmay.pillai@gmail.com>
2024-03-11 00:57:11 +0530

    Register Models on Admin Site

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import UserProfile, Property, Order, OrderBook
 
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Property)
+admin.site.register(Order)
+admin.site.register(OrderBook)
+
