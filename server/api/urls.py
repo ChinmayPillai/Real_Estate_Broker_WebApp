43b65cc9790678cc47fc6f55102f25b14f903c2b
Chinmay Pillai <chinmay.pillai@gmail.com>
2024-03-12 00:44:30 +0530

    Add getProperties API

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    #path('test', )
+    path('properties', views.property),
 ]
--- server/api/views.py
+++ server/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .models import Property
+from .serializers import PropertySerializer
 
-# Create your views here.
+@api_view(['GET'])
+def property(request):
+    property = Property.objects.all()
+    serializer = PropertySerializer(property, many=True)
+    return Response({"properties": serializer.data})
+
