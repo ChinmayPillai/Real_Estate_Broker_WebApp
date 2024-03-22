74644e3b07bdaedfe7343145c255de2e26f5b209
Chinmay Pillai <chinmay.pillai@gmail.com>
2024-03-22 14:15:15 +0530

    Add explicit type conversion for portfolio property id

--- server/api/views.py
+++ server/api/views.py
@@ -207,7 +207,7 @@ def portfolio(request, id):
             return Response({"error": "action field is required"}, status=status.HTTP_400_BAD_REQUEST)
         
         
-        property_id = request.data.get('property_id')
+        property_id = int(request.data.get('property_id'))
         if property_id is None:
             return Response({"error": "property_id field is required"}, status=status.HTTP_400_BAD_REQUEST)
 
