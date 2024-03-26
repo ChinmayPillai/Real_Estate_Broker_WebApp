58d3ce1f04f771e7d7a72010578b0c59d45893d3
Chinmay Pillai <chinmay.pillai@gmail.com>
2024-03-27 03:02:15 +0530

    Add Property Model Test

--- server/api/tests/test_models.py
+++ server/api/tests/test_models.py
@@ -5,7 +5,8 @@ class TestModels(TestCase):
     
     # Create a new user profile with all required fields
     def test_create_user_profile_with_all_required_fields(self):
-        user = UserProfile.objects.create(username="jdoe", name="John Doe", email="johndoe@example.com")
+        user = UserProfile.objects.create(username="jd", password="123", name="John Doe", email="johndoe@example.com")
+        assert user.username == "jd"
         assert user.name == "John Doe"
         assert user.email == "johndoe@example.com"
         assert user.phone == "9876543210"
@@ -15,3 +16,14 @@ class TestModels(TestCase):
         assert user.portfolio == []
         assert user.watchlist == []
         assert user.pan == "ABCDE1234F"
+
+    
+    # Property object can be created with all fields filled
+    def test_property_creation_with_all_fields_filled(self):
+        property = Property(name="Test Property", category="Test Category", description="Test Description", image="Test Image", location="Test Location", ltp=100.00)
+        assert property.name == "Test Property"
+        assert property.category == "Test Category"
+        assert property.description == "Test Description"
+        assert property.image == "Test Image"
+        assert property.location == "Test Location"
+        assert property.ltp == 100.00
