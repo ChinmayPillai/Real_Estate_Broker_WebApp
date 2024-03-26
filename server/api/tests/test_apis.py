7de3ac4b40b5db37f96d19c458987e5235af1000
Chinmay Pillai <chinmay.pillai@gmail.com>
2024-03-27 05:02:35 +0530

    Fix TestPropertyById Module

--- server/api/tests/test_apis.py
+++ server/api/tests/test_apis.py
@@ -1,3 +1,4 @@
+import json
 from django.test import RequestFactory, TestCase
 from api.models import UserProfile, Property, Order, Support
 from rest_framework import status
@@ -5,14 +6,14 @@ from api.views import register, login, marketOrder, limitOrder, property, proper
 from api.serializers import PropertySerializer
 
     
-class TestPropertyById:
+class TestPropertyById(TestCase):
 
     # The function returns a valid property object when given a valid id and a GET request.
     def test_valid_property_object(self):
         # Arrange
         id = 1
-        request = RequestFactory().get(f'/property/{id}')
-        property = Property.objects.create(name='Test Property', category='Test Category', description='Test Description', image='Test Image', location='Test Location', ltp=100.00)
+        request = RequestFactory().get(f'/properties/{id}')
+        property = Property.objects.create(id=1, name='Test Property', category='Test Category', description='Test Description', image='Test Image', location='Test Location', ltp=100.00)
         serializer = PropertySerializer(property, many=False)
     
         # Act
@@ -26,7 +27,7 @@ class TestPropertyById:
     def test_invalid_id_404(self):
         # Arrange
         id = 999
-        request = RequestFactory().get(f'/property/{id}')
+        request = RequestFactory().get(f'/properties/{id}')
     
         # Act
         response = property_by_id(request, id)
@@ -38,7 +39,7 @@ class TestPropertyById:
     def test_invalid_id_delete_request(self):
         # Arrange
         id = 9999
-        request = RequestFactory().delete(f'/property/{id}')
+        request = RequestFactory().delete(f'/properties/{id}')
     
         # Act
         response = property_by_id(request, id)
@@ -50,7 +51,8 @@ class TestPropertyById:
     def test_invalid_data_returns_400(self):
         # Arrange
         id = 1
-        request = RequestFactory().put(f'/property/{id}')
+        property = Property.objects.create(id=1, name='Test Property', category='Test Category', description='Test Description', image='Test Image', location='Test Location', ltp=100.00)
+        request = RequestFactory().put(f'/properties/{id}')
         request.data = {'name': 'Test Property', 'category': 'Test Category', 'description': 'Test Description', 'image': 'Test Image', 'location': 'Test Location', 'ltp': 'invalid'}
 
         # Act
@@ -63,8 +65,8 @@ class TestPropertyById:
     def test_valid_property_object(self):
         # Arrange
         id = 1
-        request = RequestFactory().get(f'/property/{id}')
-        property = Property.objects.create(name='Test Property', category='Test Category', description='Test Description', image='Test Image', location='Test Location', ltp=100.00)
+        request = RequestFactory().get(f'/properties/{id}')
+        property = Property.objects.create(id=1, name='Test Property', category='Test Category', description='Test Description', image='Test Image', location='Test Location', ltp=100.00)
         serializer = PropertySerializer(property, many=False)
 
         # Act
@@ -78,8 +80,8 @@ class TestPropertyById:
     def test_put_request_with_missing_data(self):
         # Arrange
         id = 1
-        request = RequestFactory().put(f'/property/{id}')
-        property = Property.objects.create(name='Test Property', category='Test Category', description='Test Description', image='Test Image', location='Test Location', ltp=100.00)
+        request = RequestFactory().put(f'/properties/{id}')
+        property = Property.objects.create(id=1, name='Test Property', category='Test Category', description='Test Description', image='Test Image', location='Test Location', ltp=100.00)
 
         # Act
         response = property_by_id(request, id)
@@ -91,8 +93,8 @@ class TestPropertyById:
     def test_delete_property_object_successfully(self):
         # Arrange
         id = 1
-        request = RequestFactory().delete(f'/property/{id}')
-        property = Property.objects.create(name='Test Property', category='Test Category', description='Test Description', image='Test Image', location='Test Location', ltp=100.00)
+        request = RequestFactory().delete(f'/properties/{id}')
+        property = Property.objects.create(id=1, name='Test Property', category='Test Category', description='Test Description', image='Test Image', location='Test Location', ltp=100.00)
 
         # Act
         response = property_by_id(request, id)
@@ -104,8 +106,8 @@ class TestPropertyById:
     def test_valid_property_object(self):
         # Arrange
         id = 1
-        request = RequestFactory().get(f'/property/{id}')
-        property = Property.objects.create(name='Test Property', category='Test Category', description='Test Description', image='Test Image', location='Test Location', ltp=100.00)
+        request = RequestFactory().get(f'/properties/{id}')
+        property = Property.objects.create(id=1, name='Test Property', category='Test Category', description='Test Description', image='Test Image', location='Test Location', ltp=100.00)
         serializer = PropertySerializer(property, many=False)
 
         # Act
@@ -119,10 +121,22 @@ class TestPropertyById:
     def test_valid_property_update(self):
         # Arrange
         id = 1
-        request = RequestFactory().put(f'/property/{id}', data={'name': 'Updated Property', 'category': 'Updated Category', 'description': 'Updated Description', 'image': 'Updated Image', 'location': 'Updated Location', 'ltp': 200.00})
-        property = Property.objects.create(name='Test Property', category='Test Category', description='Test Description', image='Test Image', location='Test Location', ltp=100.00)
+        request_data = {
+        'name': 'Updated Property',
+        'category': 'Updated Category',
+        'description': 'Updated Description',
+        'image': 'Updated Image',
+        'location': 'Updated Location',
+        'ltp': 200.00
+        }
+        request = RequestFactory().put(
+            f'/properties/{id}',
+            data=json.dumps(request_data),
+            content_type='application/json'
+        )
+        property = Property.objects.create(id=1, name='Test Property', category='Test Category', description='Test Description', image='Test Image', location='Test Location', ltp=100.00)
         serializer = PropertySerializer(property, many=False)
-
+        serializer.update(property, request_data)
         # Act
         response = property_by_id(request, id)
 
@@ -134,12 +148,12 @@ class TestPropertyById:
     def test_delete_valid_property_object(self):
         # Arrange
         id = 1
-        request = RequestFactory().delete(f'/property/{id}')
-        property = Property.objects.create(name='Test Property', category='Test Category', description='Test Description', image='Test Image', location='Test Location', ltp=100.00)
+        request = RequestFactory().delete(f'/properties/{id}')
+        property = Property.objects.create(id=1, name='Test Property', category='Test Category', description='Test Description', image='Test Image', location='Test Location', ltp=100.00)
 
         # Act
         response = property_by_id(request, id)
 
         # Assert
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-    
+
