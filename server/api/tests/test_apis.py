4878c494b1fa65e2dc16032284990f1ae0003685
Chinmay Pillai <chinmay.pillai@gmail.com>
2024-03-27 08:07:23 +0530

    Fix TestMarketOrder Module

--- server/api/tests/test_apis.py
+++ server/api/tests/test_apis.py
@@ -807,7 +807,7 @@ class TestMarketOrder(TestCase):
         self.property3 = Property.objects.create(id=3, name='Test Property', category='Test Category', description='Test Description', location='Test Location', ltp=100.00)
         self.property4 = Property.objects.create(id=4, name='Test Property', category='Test Category', description='Test Description', location='Test Location', ltp=100.00)
         Order.objects.create(user=self.user2, prop=self.property, price=50, order_type='sell')
-        Order.objects.create(user=self.user2, prop=self.property, price=45, order_type='buy')
+        Order.objects.create(user=self.user, prop=self.property, price=45, order_type='buy')
         Order.objects.create(user=self.user2, prop=self.property2, price=45, order_type='sell')
         Order.objects.create(user=self.user, prop=self.property3, price=50, order_type='sell')
 
@@ -827,8 +827,29 @@ class TestMarketOrder(TestCase):
         # Assert
         assert response.status_code == status.HTTP_200_OK
         assert 'portfolio' in response.data
+        assert response.data['portfolio'] == [2, 3, 1]
     
     
+    
+    # Place Market Order Buy
+    def test_sell_market_order(self):
+        # Arrange
+        data = {
+            'user_id': 2,
+            'property_id': 1,
+            'action': 'sell'
+        }
+        request = RequestFactory().put('/marketorder', data=data, content_type='application/json')
+
+        # Act
+        response = marketOrder(request)
+
+        # Assert
+        assert response.status_code == status.HTTP_200_OK
+        assert 'portfolio' in response.data
+        assert response.data['portfolio'] == []
+    
+
     def test_no_buy_orders(self):
         # Arrange
         data = {
